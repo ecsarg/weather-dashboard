703297052c5db9f6659b1fd5020412e7a70e9e76
Ellie Sargent <elliecsargent@gmail.com>
2022-05-23 19:58:44 -0500

    trying to problem solve localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,27 @@
-var cityEl = document.getElementsByClassName("currentCity");
-var cityImageEl = document.getElementsByClassName("currentImage");
-var currentTempEl = document.getElementsByClassName("currentTemp");
-var currentWindEl = document.getElementsByClassName("currentWind");
-var currentHumidityEl = document.getElementsByClassName("currentHumidity");
-var currentUVEl = document.getElementsByClassName("currentUV");
-var cityHistory = document.getElementById("history");
+var pastCities= [];
 
-var APIKey = "af367ea16009cefef0fccb4e2e9f31ee";
+var citySearch = document.querySelector(".city-search");
+var cityEl = document.querySelector(".currentCity");
+var cityImageEl = document.querySelector(".currentImage");
+var currentTempEl = document.querySelector(".currentTemp");
+var currentWindEl = document.querySelector(".currentWind");
+var currentHumidityEl = document.querySelector(".currentHumidity");
+var currentUVEl = document.querySelector(".currentUV");
+var cityHistory = document.querySelector(".pastCities");
+var searchBtn = document.querySelector(".searchBtn");
 
-var currentWeather = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey);
-    console.log(currentWeather);
+function saveCity() {
+
+    localStorage.setItem("cities", cityHistory);
 };
 
-searchBtn.on("click", currentWeather());
+
+// var APIKey = "af367ea16009cefef0fccb4e2e9f31ee";
+
+// function currentcity(cityName) {
+//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey;
+
+
+searchBtn.addEventListener("click", saveCity);
+
+
